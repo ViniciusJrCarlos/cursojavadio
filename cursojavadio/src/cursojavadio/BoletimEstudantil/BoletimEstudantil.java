eeeeed87f18eb8d3b0aefce980227571860ae3bb
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-02-23 00:04:15 -0300

    segundo exemplo

---
+++ cursojavadio/src/cursojavadio/BoletimEstudantil/BoletimEstudantil.java
@@ -0,0 +1,19 @@
+package cursojavadio.BoletimEstudantil;
+
+public class BoletimEstudantil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int mediafinal = 6;
+		
+		if(mediafinal < 6)
+			System.out.println("Aluno Aprovado...");
+		else if (mediafinal == 6)
+			System.out.println("Recuperação...");
+		else
+			System.out.println("Aluno Aprovado...");
+		
+	}
+
+}
